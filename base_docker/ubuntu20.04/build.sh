5db105b259a31da096ced441ccd663f90b9a1cb8
shandawei <shandawei@me.com>
2023-06-05 00:07:06 +0800

    修复bug

--- base_docker/ubuntu20.04/build.sh
+++ base_docker/ubuntu20.04/build.sh
@@ -1,7 +1,7 @@
-$REPO_NAME='yuanzhibang/ubuntu20.04'
-$DOCKER_FILE='Dockerfile'
-$BUILD_SRC='.'
-$ARCH='linux/amd64,linux/arm64'
+REPO_NAME='yuanzhibang/ubuntu20.04'
+DOCKER_FILE='Dockerfile'
+BUILD_SRC='.'
+ARCH='linux/amd64,linux/arm64'
 
 
 docker buildx build --platform $ARCH -t $REPO_NAME:default -f $DOCKER_FILE $BUILD_SRC --push
