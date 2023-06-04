c71a10e969fa909a208449577bef0484ea590047
shandawei <shandawei@me.com>
2023-06-05 00:38:07 +0800

    add php7.4-debug

---
+++ base_docker/php7.4-debug/build.sh
@@ -0,0 +1,9 @@
+repo='yuanzhibang/php7.4-debug'
+dockerFile='Dockerfile'
+buildSrc='.'
+arch='linux/amd64,linux/arm64'
+currentTime=`date '+%Y%m%d%H%M'`
+
+docker buildx build --platform $arch -t $repo:default -f $dockerFile $buildSrc --push
+docker buildx build --platform $arch -t $repo:latest -f $dockerFile $buildSrc --push
+docker buildx build --platform $arch -t $repo:$currentTime -f $dockerFile $buildSrc --push
