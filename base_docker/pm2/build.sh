26d14c8716cc72a6480a9d32a31c42f6e5d69016
shandawei <shandawei@me.com>
2023-06-05 01:17:32 +0800

    删除

--- base_docker/pm2/build.sh
+++
@@ -1,9 +0,0 @@
-repo='yuanzhibang/pm2'
-dockerFile='Dockerfile'
-buildSrc='.'
-arch='linux/amd64,linux/arm64'
-currentTime=`date '+%Y%m%d%H%M'`
-
-docker buildx build --platform $arch -t $repo:default -f $dockerFile $buildSrc --push
-docker buildx build --platform $arch -t $repo:latest -f $dockerFile $buildSrc --push
-docker buildx build --platform $arch -t $repo:$currentTime -f $dockerFile $buildSrc --push
