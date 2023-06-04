1aedba3b5837a60c5d40d046179df245f975b91c
shandawei <shandawei@me.com>
2023-06-04 23:58:31 +0800

    添加构建

---
+++ base_docker/ubuntu20.04/build.sh
@@ -0,0 +1,9 @@
+$REPO_NAME='yuanzhibang/ubuntu20.04'
+$DOCKER_FILE='Dockerfile'
+$BUILD_SRC='.'
+$ARCH='linux/amd64,linux/arm64'
+
+
+docker buildx build --platform $ARCH -t $REPO_NAME:default -f $DOCKER_FILE $BUILD_SRC --push
+docker buildx build --platform $ARCH -t $REPO_NAME:latest -f $DOCKER_FILE $BUILD_SRC --push
+docker buildx build --platform $ARCH -t $REPO_NAME:$time -f $DOCKER_FILE $BUILD_SRC --push
