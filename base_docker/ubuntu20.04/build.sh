904b792ebebd1970602b2fb7da7878ff6986cfae
shandawei <shandawei@me.com>
2023-06-05 00:13:18 +0800

    FIX

--- base_docker/ubuntu20.04/build.sh
+++ base_docker/ubuntu20.04/build.sh
@@ -2,8 +2,8 @@ repo='yuanzhibang/ubuntu20.04'
 dockerFile='Dockerfile'
 buildSrc='.'
 arch='linux/amd64,linux/arm64'
-time=`date '+%Y%m%d%H%M'`
+currentTime=`date '+%Y%m%d%H%M'`
 
 docker buildx build --platform $arch -t $repo:default -f $dockerFile $buildSrc --push
 docker buildx build --platform $arch -t $repo:latest -f $dockerFile $buildSrc --push
-docker buildx build --platform $arch -t $repo:$time -f $dockerFile $buildSrc --push
+docker buildx build --platform $arch -t $repo:$currentTime -f $dockerFile $buildSrc --push
